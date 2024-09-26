97db6a8bef8ced9a6b314202bd461ad5303023f4
JaneAndersen <125027092+JaneAndersen@users.noreply.github.com>
2024-09-26 13:59:55 +0400

    Create main.go

---
+++ Lab1/lab1_2/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var (
+		int_num   int     = 12
+		float_num float64 = 11.38
+		str       string  = "golang"
+		b         bool    = true
+	)
+	fmt.Println(int_num, float_num, str, b)
+}
